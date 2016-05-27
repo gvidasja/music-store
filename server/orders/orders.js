a4f5b7f4067314cc28ac6fafc04c9a303150ef26
Gvidas Janušonis <gvidas.janusonis@adform.com>
2016-05-27 10:48:11 +0300

    more orders

--- server/orders/orders.js
+++ server/orders/orders.js
@@ -3,6 +3,11 @@ var db = require( './../database' );
 var helpers = require( './../helpers' );
 var promise = require( 'promise' );
 
+var discountTypes = {
+    percentage: 1,
+    sum: 2
+};
+
 var queries = {
     getOrders: require( './get-all.sql' ),
     getOrder: require( './get.sql' ),
@@ -63,14 +68,23 @@ function saveOrder( request, response ) {
             var data = request.body;
             data.id = data.id || rows[ 0 ].id + 1;
 
-            Promise.all(
-                db.query( getAlbum( data.album ), ( error, rows ) => rows[ 0 ] ),
-                db.query( getDiscount( data.promo ), ( error, rows ) => rows[ 0 ] )
-            ).then( responses => {
+            var promises = [
+                new promise((resolve, reject) => db.query( getAlbum( data.album ), ( error, rows ) => error ? reject(error.message) : resolve(rows[ 0 ]) ))
+            ];
+
+            if( data.promo ) {
+                promises.push(new promise((resolve, reject) => db.query( getDiscount( data.promo ), ( error, rows ) => error ? reject(error.message) : resolve(rows[ 0 ] ))));
+            }
+
+            promise.all(promises).then( responses => {
                 var album = responses[0];
                 var discount = responses[1];
 
-                if( discount.type )
+                if( discount && discount.type == discountTypes.percentage ) {
+                    data.price = Math.max( album.price * (100 - discount.amount) / 100, 0 );
+                } else if( discount && discount.type == discountTypes.sum ) {
+                    data.price = Math.max( album.price - discount.amount, 0 );
+                }
 
                 db.query( helpers.insertData( queries.saveOrder, data ), ( error, rows ) => {
                     if( error ) {
